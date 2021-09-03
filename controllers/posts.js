0f655e8364918b65f5a7d59e5ad7c794106bbeab
cspark0610 <75094281+cspark0610@users.noreply.github.com>
2021-09-02 22:48:59 -0300

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -49,3 +49,45 @@ export const getByContinentQuery = async (req, res) => {
 		res.status(404).json({ message: error.message });
 	}
 };
+
+//getBySearchQeury will be reused in update controller
+export const updateCountry = async (req, res) => {
+  //will sent as an object new values of to add
+
+ /*
+	objectSended = {
+		cases: { new: 15, active: 55 ....},
+		deaths: { new : 155, total:  .....},
+	}
+*/
+  const { country } = req.query;
+  const { cases, deaths } = req.body;
+
+  if (!country)
+    res.status(404).json({ message: "the country does not exists" });
+
+  try {
+    const foundCountry = getBySearchQuery(country);
+
+    console.log(foundCountry);
+    console.log(cases);
+    console.log(deaths);
+
+    const id = foundCountry._id;
+    const updatedCountry = {
+      _id: id,
+      continent: foundCountry.continent,
+      country: foundCountry.continent,
+      population: foundCountry.population,
+      cases: JSON.stringify(cases),
+      deaths: JSON.stringify(deaths),
+      tests: foundCountry.tests,
+      day: new Date().toISOString().split("T")[0],
+      time: new Date().toISOString()
+    };
+    await CovidPosts.findByIdAndUpdate(id, updatedCountry, { new: true });
+    res.status(204).json({ message: updatedCountry });
+  } catch (error) {
+    console.log(error);
+  }
+};
